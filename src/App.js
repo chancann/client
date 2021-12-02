f73761b7f570cdd88621f1a9691662e0f577c06d
Chandra Riyadi <chandra.riyadi@raharja.info>
2021-12-02 14:17:39 +0700

    Install routerdom & tailwindUi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      <Home/>
     </div>
   );
 }
